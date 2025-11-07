77a63973f517aaf15c8186fc82f0227646b30904
Luiz Américo <144196020+Spideco@users.noreply.github.com>
2025-11-07 13:07:19 -0300

    Change body font from 'Baloo 2' to 'Inter'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ export default {
     extend: {
       fontFamily: {
         heading: ["Inter", "system-ui", "sans-serif"],
-        body: ["Baloo 2", "system-ui", "sans-serif"],
+        body: ["Inter", "system-ui", "sans-serif"],
       },
       colors: {
         border: "hsl(var(--border))",
