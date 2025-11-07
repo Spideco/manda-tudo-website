7871eb83bc817b506d00673a819f7800111f0efc
Luiz Américo <144196020+Spideco@users.noreply.github.com>
2025-11-07 12:51:40 -0300

    Revert "Replace Index component with PaginaInicial"
    This reverts commit 63b507f9a9f00ca43e95cc3be3f4dbd742c67100.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
-import PaginaInicial from "./pages/PaginaInicial";
+import Index from "./pages/Index";
 import QuemSomos from "./pages/QuemSomos";
 import NossaHistoria from "./pages/NossaHistoria";
 import Contato from "./pages/Contato";
@@ -20,7 +20,7 @@ const App = () => (
       <BrowserRouter>
         <ScrollToTop />
         <Routes>
-          <Route path="/pagina-inicial" element={<PaginaInicial />} />
+          <Route path="/pagina-inicial" element={<Index />} />
           <Route path="/quem-somos" element={<QuemSomos />} />
           <Route path="/nossa-historia" element={<NossaHistoria />} />
           <Route path="/contato" element={<Contato />} />
