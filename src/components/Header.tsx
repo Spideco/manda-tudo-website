c31ed726d16b386e5c8992b8ac0979300c73efdc
Luiz Américo <144196020+Spideco@users.noreply.github.com>
2025-11-07 12:28:40 -0300

    Change navigation link name and path to Portuguese

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,8 +15,8 @@ const Header = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   const navLinks = [{
-    name: "Home",
-    path: "/"
+    name: "Página Inicial",
+    path: "/pagina-inicial"
   }, {
     name: "Quem Somos",
     path: "/quem-somos"
